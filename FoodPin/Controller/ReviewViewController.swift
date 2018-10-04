da3ea5359b46fd0977215be9e78b60dfb5bc3bb7
Jan Hovland <jan.hovland@lyse.net>
2018-10-04 15:52:03 +0200

    Ferdig med "Rate it", men mangler løsning på oppgave #1

--- FoodPin/Controller/ReviewViewController.swift
+++ FoodPin/Controller/ReviewViewController.swift
@@ -41,43 +41,39 @@ class ReviewViewController: UIViewController {
         
     }
   
-//    override func viewWillAppear(_ animated: Bool) {
-//        UIView.animate(withDuration: 2.0) {
-//            self.rateButtons[0].alpha = 1.0
-//            self.rateButtons[1].alpha = 1.0
-//            self.rateButtons[2].alpha = 1.0
-//            self.rateButtons[3].alpha = 1.0
-//            self.rateButtons[4].alpha = 1.0
-//        }
-//
-//    }
-
     override func viewWillAppear(_ animated: Bool) {
-        UIView.animate(withDuration: 0.4, delay: 0.1, options: [], animations: {
-            self.rateButtons[0].alpha = 1.0
-            self.rateButtons[0].transform = .identity
-        }, completion: nil)
+//        UIView.animate(withDuration: 0.4, delay: 0.1, options: [], animations: {
+//            self.rateButtons[0].alpha = 1.0
+//            self.rateButtons[0].transform = .identity
+//        }, completion: nil)
+//        
+//        UIView.animate(withDuration: 0.4, delay: 0.15, options: [], animations: {
+//            self.rateButtons[1].alpha = 1.0
+//            self.rateButtons[1].transform = .identity
+//        }, completion: nil)
+//        
+//        UIView.animate(withDuration: 0.4, delay: 0.2, options: [], animations: {
+//            self.rateButtons[2].alpha = 1.0
+//            self.rateButtons[2].transform = .identity
+//        }, completion: nil)
+//        
+//        UIView.animate(withDuration: 0.4, delay: 0.25, options: [], animations: {
+//            self.rateButtons[3].alpha = 1.0
+//            self.rateButtons[3].transform = .identity
+//        }, completion: nil)
+//        
+//        UIView.animate(withDuration: 0.4, delay: 0.3, options: [], animations: {
+//            self.rateButtons[4].alpha = 1.0
+//            self.rateButtons[4].transform = .identity
+//        }, completion: nil)
         
-        UIView.animate(withDuration: 0.4, delay: 0.15, options: [], animations: {
-            self.rateButtons[1].alpha = 1.0
-            self.rateButtons[1].transform = .identity
-        }, completion: nil)
+        for index in 0...4 {
+            UIView.animate(withDuration: 0.4, delay: (0.1 + 0.05 * Double(index)), options: [], animations: {
+                self.rateButtons[index].alpha = 1.0
+                self.rateButtons[index].transform = .identity
+            }, completion: nil)
+        }
         
-        UIView.animate(withDuration: 0.4, delay: 0.2, options: [], animations: {
-            self.rateButtons[2].alpha = 1.0
-            self.rateButtons[2].transform = .identity
-        }, completion: nil)
-        
-        UIView.animate(withDuration: 0.4, delay: 0.25, options: [], animations: {
-            self.rateButtons[3].alpha = 1.0
-            self.rateButtons[3].transform = .identity
-        }, completion: nil)
-        
-        UIView.animate(withDuration: 0.4, delay: 0.3, options: [], animations: {
-            self.rateButtons[4].alpha = 1.0
-            self.rateButtons[4].transform = .identity
-        }, completion: nil)
-
     }
     
 }
